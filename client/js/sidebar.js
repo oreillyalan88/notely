19061da8cfefb569ce926beed13d6ea7039c33b0
Alan O' Reilly <oreillyalan88@gmail.com>
2018-02-06 20:54:28 +0000

    Added logout functionality to sidebar

---
+++ client/js/sidebar.js
@@ -0,0 +1,19 @@
+Template.sidebar.rendered = function(){
+    
+}
+
+Template.sidebar.events({
+    "click.logout": function(event){
+        
+        Meteor.logout(function(err){
+            if(err){
+               Bert.alert(err.reason, "danger", "growl-top-right") 
+            }
+            else{
+               Router.go('/')
+               Bert.alert("You Are Now Logged Out", "danger", "growl-top-right") 
+            }
+        })
+    },
+    
+})
