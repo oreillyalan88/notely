209b7ff6f3154045d278226e845546d18af02462
Alan O' Reilly <oreillyalan88@gmail.com>
2018-03-12 16:14:24 +0000

    Reversed date format in addPots so livestamp.js would show correct time since post

--- client/js/postForm.js
+++ client/js/postForm.js
@@ -3,29 +3,29 @@ Template.postForm.rendered = function(){
 }
 
 Template.postForm.events({
-    "submit .form-post":function(){
+    "submit .form-post":function(event){
         var postSubject = event.target.postSubject.value;
         var postInput = event.target.postInput.value;
         
         if(isNotEmpty(postSubject) &&
-               isNotEmpty(postInput)){
+              isNotEmpty(postInput)){
                    
-               Meteor.call('addPosts', postSubject, postInput)    
+              Meteor.call('addPosts', postSubject, postInput)    
                
-               event.target.postSubject.value ="";
-               event.target.postInput.value ="";
+              event.target.postSubject.value ="";
+              event.target.postInput.value ="";
                    
-               Bert.alert("Your Post Was Succesful!", "success", "growl-top-right")   
+              Bert.alert("Your Post Was Succesful!", "success", "growl-top-right")   
                
          } else {
-               Bert.alert("Error Occured", "danger", "growl-top-right")
+              Bert.alert("Error Occured", "danger", "growl-top-right")
                 
          }
                         
          return false;
 
         
-    }
+    },
     
 })
 
