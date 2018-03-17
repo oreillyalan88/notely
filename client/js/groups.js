0dafe13a944a30c8147e8b4036490f713f870840
Alan O' Reilly <oreillyalan88@gmail.com>
2018-03-17 13:57:13 +0000

    added group and mygroup layouts

---
+++ client/js/groups.js
@@ -0,0 +1,12 @@
+Template.groups.rendered = function(){
+    
+        
+    $("#posts-link").removeClass('selected')
+    $("#profile-link").removeClass('selected')
+    $("#rankings-link").removeClass('selected')
+    $("#search-link").removeClass('selected')
+    $("#login-link").removeClass('selected')
+    $("#group-link").addClass('selected')
+  
+    
+}
---
+++ client/js/mygroups.js
@@ -0,0 +1,13 @@
+Template.mygroups.rendered = function(){
+    
+        
+    $("#posts-link").removeClass('selected')
+    $("#profile-link").removeClass('selected')
+    $("#rankings-link").removeClass('selected')
+    $("#search-link").removeClass('selected')
+    $("#login-link").removeClass('selected')
+    $("#mygroups-link").addClass('selected')
+    $("#group-link").removeClass('selected')
+  
+    
+}
