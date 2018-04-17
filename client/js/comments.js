ec0d12d628da3977a89ba5d9354899e1d42007cb
Alan O' Reilly <oreillyalan88@gmail.com>
2018-04-17 23:19:59 +0100

    Users and Admins can now remove comments from posts

--- client/js/comments.js
+++ client/js/comments.js
@@ -7,11 +7,12 @@ Template.commentForm.events({
         console.log('i am here')
         var tempPostId = template.data._id;
         var commentInput = event.target.commentInput.value;
+        var commentId = makeId
 
         if(isNotEmpty(commentInput)
              ){
                    
-            Meteor.call('addComments', commentInput, tempPostId)    
+            Meteor.call('addComments', commentInput, tempPostId, commentId())
                
               event.target.commentInput.value ="";
                    
@@ -26,5 +27,75 @@ Template.commentForm.events({
         return false;
         
     },
-    
+
+
 })
+Template.comments.events({
+    "click #deleteCommentButton": function (event, template) {
+        var userId = Meteor.userId()
+
+        var commentId = this.commentId
+
+        Meteor.call('removeComment', userId, commentId, function (err) {
+                if (err) {
+                    Bert.alert("Error Occurred", "danger", "growl-top-right")
+                }
+
+                else {
+                    Bert.alert("Comment Removed", "success", "growl-top-right")
+                }
+
+            }
+        )
+        return false;
+
+
+    },
+
+    "click #adminDeleteCommentButton": function (event, template) {
+        var userId = this.userId
+
+        var commentId = this.commentId
+
+        Meteor.call('removeComment', userId, commentId, function (err) {
+                if (err) {
+                    Bert.alert("Error Occurred", "danger", "growl-top-right")
+                }
+
+                else {
+                    Bert.alert("Comment Removed", "success", "growl-top-right")
+                }
+
+            }
+        )
+        return false;
+
+
+    },
+})
+
+
+Template.comments.helpers({
+    postOwner:function(){
+
+        var postOwner =  this.userId
+        var thisUser = Meteor.userId()
+
+        if (postOwner == thisUser){
+            return true
+        }
+        else return false
+
+                       },
+
+})
+
+makeId= function() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 10; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
